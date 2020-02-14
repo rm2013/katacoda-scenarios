8fe89943c90ca18ad01e476ae4d71d19025b76c8
Rashid Mohammad <rmohamma@CHI-ML-602073.local>
2020-02-13 23:54:14 -0600

    Correct file path

--- hello-kafka/init.sh
+++ hello-kafka/init.sh
@@ -3,20 +3,20 @@ mkdir /opt/zookeeper
 mkdir /opt/kafka
 
 
-mv docker-storageclass-broker.yml storage-class/
-mv docker-storageclass-zookeeper.yml storage-class/
+mv /opt/docker-storageclass-broker.yml storage-class/
+mv /opt/docker-storageclass-zookeeper.yml storage-class/
 
-mv 10broker-config.yml kafka/
-mv 20dns.yml kafka/
-mv 30bootstrap-service.yml kafka/
-mv 50kafka.yml kafka/
+mv /opt/10broker-config.yml kafka/
+mv /opt/20dns.yml kafka/
+mv /opt/30bootstrap-service.yml kafka/
+mv /opt/50kafka.yml kafka/
 
-mv 10zookeeper-config.yml zookeeper/
-mv 20pzoo-service.yml zookeeper/
-mv 30service.yml zookeeper/
-mv 21zoo-service.yml zookeeper/
-mv 50pzoo.yml zookeeper/
-mv 51zoo.yml zookeeper/
+mv /opt/10zookeeper-config.yml zookeeper/
+mv /opt/20pzoo-service.yml zookeeper/
+mv /opt/30service.yml zookeeper/
+mv /opt/21zoo-service.yml zookeeper/
+mv /opt/50pzoo.yml zookeeper/
+mv /opt/51zoo.yml zookeeper/
 
 /usr/bin/launch.sh 
 
