4162c3abe8e6903cd21a7c307ee224bc919dff2c
Rashid Mohammad <rmohamma@CHI-ML-602073.local>
2020-02-21 21:05:18 -0600

    Add init.sh

--- hello-kafka/init.sh
+++ hello-kafka/init.sh
@@ -1,23 +1,20 @@
 
+#Launch Kubernetes
 /usr/bin/launch.sh 
 
 cd /opt
 
+#Make dir for kafka data
 mkdir /opt/kafka
-
 mkdir /opt/kafka/data
 
+#Make dir for Persistence zookeeper and kafka deployments
 mkdir /opt/zkp
-
 mkdir /opt/kfk
-
 mkdir /opt/pv
 
 
-ls /opt/1*
-
+#Copy respectives files into deployment dirs
 mv /opt/1* /opt/zkp
-
 mv /opt/2* /opt/kfk
-
 mv /opt/0* /opt/pv
