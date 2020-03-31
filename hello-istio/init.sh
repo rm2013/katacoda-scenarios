139f9e2a755cf6b74a1eee97afee1030f083fd02
Rashid Mohammad <rmohamma@CHI-ML-602155.local>
2020-03-30 23:37:50 -0500

    Add cluster

--- hello-istio/init.sh
+++ hello-istio/init.sh
@@ -2,46 +2,46 @@
 #Launch Kubernetes
 /usr/bin/launch.sh 
 
-cd /opt
+#cd /opt
 
 #Make dir for kafka data
-mkdir /opt/kafka
-mkdir /opt/kafka/data
+#mkdir /opt/kafka
+#mkdir /opt/kafka/data
 
 #Make dir for Persistence zookeeper and kafka deployments
-mkdir /opt/zkp
-mkdir /opt/kfk
-mkdir /opt/pv
+#mkdir /opt/zkp
+#mkdir /opt/kfk
+#mkdir /opt/pv
 
 
 #Copy respectives files into deployment dirs
-mv /opt/1* /opt/zkp
-mv /opt/2* /opt/kfk
-mv /opt/0* /opt/pv
+#mv /opt/1* /opt/zkp
+#mv /opt/2* /opt/kfk
+#mv /opt/0* /opt/pv
 
 #Create a namespace
-kubectl create namespace testspace
+#kubectl create namespace testspace
 
 #Deploy storagewclass and persistent volume
-kubectl apply -f /opt/pv
-sleep 60
+#kubectl apply -f /opt/pv
+#sleep 60
  
 #Deploy zookeeper service and pods
-kubectl apply -f /opt/zkp
-sleep 60
+#kubectl apply -f /opt/zkp
+#sleep 60
 
 #Wait for the zookeeper pod to come up
-kubectl -ntestspace get po
+#kubectl -ntestspace get po
 
 #Deploy kafka service and tpods
 
-kubectl apply -f /opt/kfk
+#kubectl apply -f /opt/kfk
 
-sleep 60
+#sleep 60
 
 #Wait for  the kafka pod to come up
 
- kubectl -ntestspace get po
+# kubectl -ntestspace get po
 
 #Display the content of  kafka-producer-consumer.yml file
 
@@ -49,10 +49,10 @@ sleep 60
 
 #Deploy kafka service and tpods
 
-kubectl apply -f /opt/kafka-producer-consumer.yml
+#kubectl apply -f /opt/kafka-producer-consumer.yml
 
 #Wait for the kafka-producer-consumer pod to come up
 
-kubectl -ntestspace get po
+#kubectl -ntestspace get po
 
 
